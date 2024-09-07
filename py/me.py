5c1fd95cd775f25fb368294ad2a73e52d226cd02
Paul Harriet Asiñero <harriebird@yahoo.com>
2024-09-08 02:13:54 +0800

    Added initial main page

---
+++ py/me.py
@@ -0,0 +1,24 @@
+# About me file
+me = {
+    "name": "Paul Harriet Asiñero",
+    "github": "https://github.com/harriebird/",
+    "linkedin": "https://www.linkedin.com/in/harriebird",
+    "medium": "https://medium.com/@harriebird",
+    "groups": [
+        {
+            "name": "Django Girls CDO",
+            "role": "Workshop Coach",
+            "link": "https://www.facebook.com/pineapplepy.ph"
+        },
+        {
+            "name": "PineapplePy",
+            "role": "Member",
+            "link": "https://www.facebook.com/pineapplepy.ph"
+        },
+        {
+            "name": "Hackm3",
+            "role": "Member",
+            "link": "https://www.facebook.com/hackm3.ph"
+        },
+    ]
+}

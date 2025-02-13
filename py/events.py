19f856d00f05b7a1345dc2ee9db907e2e89aec14
Paul Harriet Asiñero <harriebird@yahoo.com>
2025-02-13 17:02:18 +0800

    Added events tab

---
+++ py/events.py
@@ -0,0 +1,149 @@
+# Events Attended file
+# by Paul Harriet Asiñero
+
+events_attended = [
+    {
+        "name": "Cybersec Asia X TICW 2025",
+        "role": "Participant",
+        "venue": "Queen Sirikit National Convention Center, Bangkok, Thailand",
+        "date": "January 22-23, 2025"
+    },
+    {
+        "name": "TAITRONICS & AIoT Taiwan 2024",
+        "role": "Participant",
+        "venue": "Taipei Nangang Exhibition Center, Taipei, Taiwan",
+        "date": "October 23-25, 2024"
+    },
+    {
+        "name": "TPCA Show 2024",
+        "role": "Participant",
+        "venue": "Taipei Nangang Exhibition Center, Taipei, Taiwan",
+        "date": "October 23-25, 2024"
+    },
+    {
+        "name": "Taiwan Innotech Expo 2024",
+        "role": "Participant",
+        "venue": "TWTC Exhibition Hall, Taipei, Taiwan",
+        "date": "October 17-19, 2024"
+    },
+    {
+        "name": "PyConf Mini Davao 2024",
+        "role": "Speaker",
+        "venue": "Ateneo de Davao University",
+        "date": "September 26, 2024"
+    },
+    {
+        "name": "Django Girls CDO 2024 Workshop",
+        "role": "Workshop Coach",
+        "venue": "DICT Training Hall, Cagayan de Oro",
+        "date": "March 9, 2024"
+    },
+    {
+        "name": "AWS Community Day - Davao Roadshow",
+        "role": "Participant",
+        "venue": "Rogen Inn, Davao City",
+        "date": "September 23, 2023"
+    },
+    {
+        "name": "Django Girls CDO 2023 Workshop",
+        "role": "Workshop Coach",
+        "venue": "DICT Training Hall, Cagayan de Oro",
+        "date": "March 4, 2023"
+    },
+    {
+        "name": "Emerging Trends in Information Technology and Computing",
+        "role": "Speaker",
+        "venue": "Central Mindanao University (Online)",
+        "date": "April 29, 2022"
+    },
+    {
+        "name": "From Studentry to Industry: The Do's, Don'ts and What If's",
+        "role": "Speaker",
+        "venue": "MSU - Iligan Institute of Technology (Online)",
+        "date": "November 28, 2020"
+    },
+    {
+        "name": "Django Girls CDO goes to Iligan City",
+        "role": "Workshop Coach, Co-organizer",
+        "venue": "MSU - Iligan Institute of Technology",
+        "date": "January 25, 2020"
+    },
+    {
+        "name": "Software Freedom Day Iligan 2019",
+        "role": "Speaker",
+        "venue": "MSU - Iligan Institute of Technology",
+        "date": "September 21, 2019"
+    },
+    {
+        "name": "Django Girls CDO goes to Bukidnon",
+        "role": "Workshop Coach",
+        "venue": "Central Mindanao University",
+        "date": "August 17, 2019"
+    },
+    {
+        "name": "Campus DevCon @ CU",
+        "role": "Speaker",
+        "venue": "Capitol University",
+        "date": "August 10, 2019"
+    },
+    {
+        "name": "alert(‘Cybersecurity Awareness’);",
+        "role": "Speaker",
+        "venue": "Northern Bukidnon Community College",
+        "date": "March 8, 2019"
+    },
+    {
+        "name": "PyCon APAC 2019",
+        "role": "Django Girls Workshop Coach / Participant",
+        "venue": "iACADEMY Nexus, Makati City",
+        "date": "February 23-24, 2019"
+    },
+    {
+        "name": "ICT Week 2019",
+        "role": "Speaker",
+        "venue": "Bukidnon State University",
+        "date": "February 1, 2019"
+    },
+    {
+        "name": "Django Girls CDO 2019 Workshop",
+        "role": "Workshop Coach",
+        "venue": "DICT Training Hall, Cagayan de Oro",
+        "date": "January 19, 2019"
+    },
+    {
+        "name": "DevCon Summit 2018",
+        "role": "Panelist",
+        "venue": "Mini Theater, Capitol University",
+        "date": "November 10, 2018"
+    },
+    {
+        "name": "Django Girls CDO 2018 Workshop",
+        "role": "Workshop Coach",
+        "venue": "DICT Training Hall, Cagayan de Oro",
+        "date": "June 30, 2018"
+    },
+    {
+        "name": "Campus DevCon @ MSU - IIT",
+        "role": "Speaker",
+        "venue": "MSU - Iligan Institute of Technology",
+        "date": "May 5, 2018"
+    },
+    {
+        "name": "1st PyTsada RoadCamp",
+        "role": "Speaker",
+        "venue": "Mindanao State University - Naawan",
+        "date": "April 7, 2018"
+    },
+    {
+        "name": "IT Days 2018",
+        "role": "Speaker",
+        "venue": "Christ the King College, Gingoog City",
+        "date": "February 24, 2018"
+    },
+    {
+        "name": "SCS Developer and Cybersecurity Workshop",
+        "role": "Speaker",
+        "venue": "MSU - Iligan Institute of Technology",
+        "date": "December 10, 2017"
+    }
+]
